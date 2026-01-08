f20c947f62ba7d039f030c2445b72e5ca1303b24
cankutukoglu <cankutukoglu03@gmail.com>
2026-01-08 18:44:26 +0300

    Fix CI: remove typo files, fix RuboCop issues, fix tests, remove --ensure-latest from brakeman

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,12 +2,14 @@ require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get users_new_url
+    get new_user_url
     assert_response :success
   end
 
-  test "should get create" do
-    get users_create_url
-    assert_response :success
+  test "should create user" do
+    assert_difference("User.count") do
+      post users_url, params: { user: { email_address: "test@example.com", password: "password123", password_confirmation: "password123", username: "testuser", user_type: "regular" } }
+    end
+    assert_redirected_to new_session_path
   end
 end
